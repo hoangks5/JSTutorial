a3f0dc5a6dbc3a93286b0a007d0a856b57332581
hoangks5 <hoangnd.ute@gmail.com>
2022-08-13 11:43:01 +0700

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+//setTimeout(function() {
+//    alert('Thong bao')
+//},5000)
+
+
+//setInterval(function() {
+//    console.log('Day la log'+Math.random())
+//},1000)
+
+var a = 6
+console.log(++a)
